2d710b09a980caf05d47dbfb379371244fb05073
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2013-07-11 12:22:29 +0200

    Fixed formatting

--- api/src/main/java/org/openmrs/util/OpenmrsConstants.java
+++ api/src/main/java/org/openmrs/util/OpenmrsConstants.java
@@ -1167,7 +1167,8 @@ public final class OpenmrsConstants {
 		props.add(new GlobalProperty(GLOBAL_PROPERTY_LOCATION_WIDGET_TYPE, "default",
 		        "Type of widget to use for location fields"));
 		
-		props.add(new GlobalProperty(GP_MAIL_SMTP_STARTTLS_ENABLE, "false", "Set to true to enable TLS encryption, else set to false"));
+		props.add(new GlobalProperty(GP_MAIL_SMTP_STARTTLS_ENABLE, "false",
+		        "Set to true to enable TLS encryption, else set to false"));
 		
 		String standardRegimens = "<list>" + "  <regimenSuggestion>" + "    <drugComponents>" + "      <drugSuggestion>"
 		        + "        <drugId>2</drugId>" + "        <dose>1</dose>" + "        <units>tab(s)</units>"
